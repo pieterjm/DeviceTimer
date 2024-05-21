3e95417bcf8d83529a2cd2fc67de63543bcafb6a
DoktorShift <106493492+DoktorShift@users.noreply.github.com>
2024-05-21 16:08:57 +0200

    Update __init__.py
    Properly-start-stop-task.
    
    Needed cause of changes in lnbits v0.12.6

--- __init__.py
+++ __init__.py
@@ -2,17 +2,16 @@ import asyncio
 from typing import List
 
 from fastapi import APIRouter
+from loguru import logger
 from fastapi.staticfiles import StaticFiles
 
 from lnbits.db import Database
 from lnbits.helpers import template_renderer
-from lnbits.tasks import catch_everything_and_restart
+from lnbits.tasks import create_permanent_unique_task
 
 db = Database("ext_devicetimer")
 
 devicetimer_ext: APIRouter = APIRouter(prefix="/devicetimer", tags=["devicetimer"])
-
-scheduled_tasks: List[asyncio.Task] = []
 
 devicetimer_static_files = [
     {
@@ -32,8 +31,15 @@ from .tasks import wait_for_paid_invoices
 from .views import *  # noqa: F401,F403
 from .views_api import *  # noqa: F401,F403
 
+scheduled_tasks: list[asyncio.Task] = []
+
+def devicetimer_stop():
+    for task in scheduled_tasks:
+        try:
+            task.cancel()
+        except Exception as ex:
+            logger.warning(ex)
 
 def devicetimer_start():
-    loop = asyncio.get_event_loop()
-    task = loop.create_task(catch_everything_and_restart(wait_for_paid_invoices))
+    task = create_permanent_unique_task("ext_devicetimer", wait_for_paid_invoices)
     scheduled_tasks.append(task)
